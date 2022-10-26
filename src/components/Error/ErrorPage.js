ea66688ab1b3e976ca7a3172b80164094019607a
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-10-26 21:30:04 +0600

    404 route complete

--- src/components/Error/ErrorPage.js
+++ src/components/Error/ErrorPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className='container error'>
             <h1>404</h1>
             <h3>Page Not Found</h3>
             <p>The page you're looking for does bot seem to exist</p>
